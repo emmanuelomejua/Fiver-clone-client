d8813626527b1ff13f851e664ea0d3e135807b90
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-12-14 18:43:38 +0000

    login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SERVER from '../../utils/server'
+import { useNavigate } from 'react-router-dom';
 import './login.scss';
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const res = await SERVER.post("/auth/login", { email, password });
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/")
+    } catch (err: any) {
+      setError(err.response.data);
+    }
+  };
+
   return (
-    <div>
-      Login
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && error}
+      </form>
     </div>
   )
 }
---
+++ src/utils/server.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const SERVER = axios.create({
+    baseURL: '',
+
+    
+})
+
+export default SERVER;
